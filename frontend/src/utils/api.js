14c8d44d627c1cbd12f842c734c22b4fb15773f1
AliZain497 <alizainsiyal497@gmail.com>
2025-08-23 09:30:22 -0700

    Connected frontend to backend API & updated base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // apka backend URL
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 export default API;
